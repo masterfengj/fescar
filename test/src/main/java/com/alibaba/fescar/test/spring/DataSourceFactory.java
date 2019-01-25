apache/incubator-seata

补充spring集成测试用例

spring/src/test/java/com/alibaba/fescar/spring/test/DataSourceFactory.java
@@ -1,0 +1,138 @@
+package com.alibaba.fescar.spring.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.fescar.rm.datasource.DataSourceProxy;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DataSourceFactory {
+
+    public static DataSource createDataSource() {
+        Properties properties = new Properties();
+        try {
+            InputStream in = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource.properties");
+            properties.load(in);
+            InputStream localIn = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource-local.properties");
+            if (localIn != null) {
+                properties.load(localIn);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("read datasource.properties error", e);
+        }
+
+        String dbUrl = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String driverClassName = properties.getProperty("driverClassName");
+        int initialSize = Integer.parseInt(properties.getProperty("initialSize"));
+        int minIdle = Integer.parseInt(properties.getProperty("minIdle"));
+        int maxActive = Integer.parseInt(properties.getProperty("maxActive"));
+        int maxWait = Integer.parseInt(properties.getProperty("maxWait"));
+        int timeBetweenEvictionRunsMillis = Integer.parseInt(properties.getProperty("timeBetweenEvictionRunsMillis"));
+        int minEvictableIdleTimeMillis = Integer.parseInt(properties.getProperty("minEvictableIdleTimeMillis"));
+        String validationQuery = properties.getProperty("validationQuery");
+        boolean testWhileIdle = Boolean.parseBoolean(properties.getProperty("testWhileIdle"));
+        boolean testOnBorrow = Boolean.parseBoolean(properties.getProperty("testOnBorrow"));
+        boolean testOnReturn = Boolean.parseBoolean(properties.getProperty("testOnReturn"));
+        String filters = properties.getProperty("filters");
+
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            throw new RuntimeException("druid configuration initialization filter", e);
+        }
+        return datasource;
+    }
+
+
+    public static DataSourceProxy createDataSourceProxy() {
+        Properties properties = new Properties();
+        try {
+            InputStream in = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource.properties");
+            properties.load(in);
+            //datasource-local.properties的配置具有更高优先级
+            //通常可以将datasource-local.properties排除在版本管理中,以便开发者有自己的数据库配置
+            InputStream localIn = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource-local.properties");
+            if (localIn != null) {
+                properties.load(localIn);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("read datasource.properties error", e);
+        }
+
+        String dbUrl = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String driverClassName = properties.getProperty("driverClassName");
+        int initialSize = Integer.parseInt(properties.getProperty("initialSize"));
+        int minIdle = Integer.parseInt(properties.getProperty("minIdle"));
+        int maxActive = Integer.parseInt(properties.getProperty("maxActive"));
+        int maxWait = Integer.parseInt(properties.getProperty("maxWait"));
+        int timeBetweenEvictionRunsMillis = Integer.parseInt(properties.getProperty("timeBetweenEvictionRunsMillis"));
+        int minEvictableIdleTimeMillis = Integer.parseInt(properties.getProperty("minEvictableIdleTimeMillis"));
+        String validationQuery = properties.getProperty("validationQuery");
+        boolean testWhileIdle = Boolean.parseBoolean(properties.getProperty("testWhileIdle"));
+        boolean testOnBorrow = Boolean.parseBoolean(properties.getProperty("testOnBorrow"));
+        boolean testOnReturn = Boolean.parseBoolean(properties.getProperty("testOnReturn"));
+        String filters = properties.getProperty("filters");
+
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            throw new RuntimeException("druid configuration initialization filter", e);
+        }
+        DataSourceProxy dataSourceProxy = new DataSourceProxy(datasource);
+        return dataSourceProxy;
+    }
+
+
+    public static JdbcTemplate createJdbcTemplate() {
+        DataSourceProxy dataSourceProxy = createDataSourceProxy();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSourceProxy);
+        return jdbcTemplate;
+    }
+
+    public static JdbcTemplate createRawJdbcTemplate() {
+        DataSource dataSource = createDataSource();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
+
+}

spring/src/test/java/com/alibaba/fescar/spring/test/Order.java
@@ -1,0 +1,38 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fescar.spring.test;
+
+import java.io.Serializable;
+
+public class Order implements Serializable {
+    public long id;
+    public String userId;
+    public String commodityCode;
+    public int count;
+    public int money;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", commodityCode='" + commodityCode + '\'' +
+                ", count=" + count +
+                ", money=" + money +
+                '}';
+    }
+}

masterfengj/fescar

test/src/main/java/com/alibaba/fescar/test/spring/DataSourceFactory.java
@@ -1,0 +1,92 @@
+package com.alibaba.fescar.test.spring;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.fescar.rm.datasource.DataSourceProxy;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DataSourceFactory {
+
+    public static DruidDataSource createDataSource() {
+        Properties properties = new Properties();
+        try {
+            InputStream in = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource.properties");
+            properties.load(in);
+            //datasource-local.properties的配置具有更高优先级
+            //通常可以将datasource-local.properties排除在版本管理中,以便开发者有自己的数据库配置
+            InputStream localIn = DataSourceFactory.class.getClassLoader().getResourceAsStream("datasource-local.properties");
+            if (localIn != null) {
+                properties.load(localIn);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("read datasource.properties error", e);
+        }
+
+        String dbUrl = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String driverClassName = properties.getProperty("driverClassName");
+        int initialSize = Integer.parseInt(properties.getProperty("initialSize"));
+        int minIdle = Integer.parseInt(properties.getProperty("minIdle"));
+        int maxActive = Integer.parseInt(properties.getProperty("maxActive"));
+        int maxWait = Integer.parseInt(properties.getProperty("maxWait"));
+        int timeBetweenEvictionRunsMillis = Integer.parseInt(properties.getProperty("timeBetweenEvictionRunsMillis"));
+        int minEvictableIdleTimeMillis = Integer.parseInt(properties.getProperty("minEvictableIdleTimeMillis"));
+        String validationQuery = properties.getProperty("validationQuery");
+        boolean testWhileIdle = Boolean.parseBoolean(properties.getProperty("testWhileIdle"));
+        boolean testOnBorrow = Boolean.parseBoolean(properties.getProperty("testOnBorrow"));
+        boolean testOnReturn = Boolean.parseBoolean(properties.getProperty("testOnReturn"));
+        String filters = properties.getProperty("filters");
+
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            throw new RuntimeException("druid configuration initialization filter", e);
+        }
+        return datasource;
+    }
+
+
+    public static DataSourceProxy createDataSourceProxy() {
+        DruidDataSource dataSource=createDataSource();
+        return new DataSourceProxy(dataSource);
+    }
+
+
+    public static JdbcTemplate createJdbcTemplate() {
+        DataSourceProxy dataSourceProxy = createDataSourceProxy();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSourceProxy);
+        return jdbcTemplate;
+    }
+
+    public static JdbcTemplate createRawJdbcTemplate() {
+        DataSource dataSource = createDataSource();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
+
+}

test/src/main/java/com/alibaba/fescar/test/spring/Order.java
@@ -1,0 +1,38 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fescar.test.spring;
+
+import java.io.Serializable;
+
+public class Order implements Serializable {
+    public long id;
+    public String userId;
+    public String commodityCode;
+    public int count;
+    public int money;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", commodityCode='" + commodityCode + '\'' +
+                ", count=" + count +
+                ", money=" + money +
+                '}';
+    }
+}